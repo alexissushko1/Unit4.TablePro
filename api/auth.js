34bcb2667d928a4b89d80775758fe215801e889a
Alexis Sushko <alexis.sushko@gmail.com>
2024-10-16 15:15:57 -0400

    Updated code

--- api/auth.js
+++ api/auth.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET;
+
 // TODO: Import jwt and JWT_SECRET
 
 // TODO: createToken
 
 const prisma = require("../prisma");
+
+const createToken = (id) => {
+  return jwt.sign({ id }, JWT_SECRET, { expiresIn: "1d" });
+};
 
 // This token-checking middleware should run before any other routes.
 // It's the first in this file, and this router is imported first in `server.js`.
@@ -16,11 +23,43 @@ router.use(async (req, res, next) => {
   if (!token) return next();
 
   // TODO: Find customer with ID decrypted from the token and attach to the request
+  try {
+    const { id } = jwt.verify(token, JWT_SECRET);
+    const customer = await prisma.customer.findUniqueOrThrow({
+      where: { id },
+    });
+    req.customer = customer;
+    next();
+  } catch (e) {
+    next(e);
+  }
 });
 
 // TODO: POST /register
 
+app.post("/register", async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const customer = await prisma.customer.register(email, password);
+    const token = createToken(customer.id);
+    res.status(201).json({ token });
+  } catch (e) {
+    next(e);
+  }
+});
+
 // TODO: POST /login
+
+app.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const customer = await prisma.customer.login(email, password);
+    const token = createToken(customer.id);
+    res.json({ token });
+  } catch (e) {
+    next(e);
+  }
+});
 
 /** Checks the request for an authenticated customer. */
 function authenticate(req, res, next) {
--- prisma/index.js
+++ prisma/index.js
@@ -5,6 +5,30 @@ const prisma = new PrismaClient().$extends({
   model: {
     customer: {
       // TODO: Add register and login methods
+
+      //Registers a new user with an email and a password where
+      //password is a hashed password
+
+      register: {
+        async register(email, password) {
+          const hashedPassword = await bcrypt.hash(password, 10);
+          const customer = await prisma.customer.create({
+            data: { email, password: hashedPassword },
+          });
+          return customer;
+        },
+      },
+
+      //Logs in a user if email and password matches
+
+      async login(email, password) {
+        const customer = await prisma.customer.findUniqueOrThrow({
+          where: { email },
+        });
+        const valid = await bcrypt.compare(password, customer.password);
+        if (!valid) throw Error("Invalid password");
+        return customer;
+      },
     },
   },
 });
